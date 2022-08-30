6ea5593bd0b622f52ce31edea4210cc4f465664b
Maria <naplesaries2014@gmail.com>
2022-08-30 10:42:35 +0200

    put corrections

--- client/src/components/add-div.js
+++ client/src/components/add-div.js
@@ -20,3 +20,21 @@ export function showUsers(data) {
 		result.append(container);
 	});
 }
+
+export function showUser(user) {
+	//clean previous result
+	const result = document.getElementById(OUTPUT_ID);
+	while (result.firstChild) {
+		result.firstChild.remove();
+	}
+	//iterate users and create new DOM elements to display them
+		const container = document.createElement('div');
+		container.classList.add('user');
+		const hEl = document.createElement('h2');
+		hEl.innerHTML = `${user.id}: ${user.name}`;
+		const pEl = document.createElement('p');
+		pEl.innerHTML = `Username:${user.username}<br>Email: ${user.email}<br>Phone: ${user.phone}
+        <br>Company: ${user.company.name}<br>Website: <a href="url">${user.website}</a><br>City: ${user.address.city}<br>Zipcode: ${user.address.zipcode}`;
+		container.append(hEl, pEl);
+		result.append(container);
+}
--- client/src/listeners/listeners.js
+++ client/src/listeners/listeners.js
@@ -1,10 +1,15 @@
 import { EVENT_ID } from "../../data/constants.js";
 import { getUser, getUsers } from "../handlers/handler.js";
 
+const userInput = document.getElementById('id')
 export const getAllListener = (element) => {
 	element.addEventListener(EVENT_ID, getUsers);
 };
 
 export const getUserListener = (element) => {
-	element.addEventListener(EVENT_ID, getUser);
+	element.addEventListener(EVENT_ID, function (e) {
+		// e.preventDefault();
+		const userId = userInput.value;
+		getUser(userId)
+	});
 };
